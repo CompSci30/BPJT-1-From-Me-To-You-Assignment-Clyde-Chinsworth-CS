d71e174590dfb15e8fe9ce73ec024db30e118c0d
Alex Yang <a.yang2@share.epsb.ca>
2018-03-10 18:12:25 +0000

    Added letter adresses

--- FromMeToYou.java
+++ FromMeToYou.java
@@ -4,6 +4,9 @@ public class Tester
 {
 	public static void main(String args[])
 	{
-	
+		System.out.println("From: Bill Smith");
+		System.out.println("Address: Dell Computer, Bldg 13");
+		System.out.println("Date: April 12, 2005");
+		System.out.println();
 	}
 }
